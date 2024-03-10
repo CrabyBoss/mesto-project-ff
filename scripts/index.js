9e8e172b65baaa24a70315b1a11115e968fb6286
Nikita Tolstov <Crabyboss@yandex.ru>
2024-03-10 22:32:04 +1000

    commiiiit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,25 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const mainContent = document.querySelector('.content');
+const cardsContainer = mainContent.querySelector('.places__list');
+
+function createCard(cardInf, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__title').textContent = cardInf.name;
+    cardElement.querySelector('.card__image').setAttribute('src', cardInf.link);
+
+    cardsContainer.append(cardElement);
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', evt => deleteCard(evt));
+}
+
+function deleteCard(evt) {
+    let card = evt.target.closest('.card');
+    card.remove();
+}
+
+initialCards.forEach(card => createCard(card, deleteCard));
